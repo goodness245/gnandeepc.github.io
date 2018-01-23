7ea2928ac1cb176ffb2e2d5ff5ddf47bec2fa420
Gnandeep Chintala <gnandeep.c25@gmail.com>
2018-01-23 22:11:25 +0000

    Add geometeric_pattern project

--- geometeric_pattern/main.js
+++ geometeric_pattern/main.js
@@ -6,6 +6,18 @@ function setup() {
     circleDiameter = width/NUM_CIRCLES;
 }
 function draw() {
+    var y = 0;
+    while (y <= height) {
+        
+    var x = 0;
+    while(x <= width) {
+        ellipse(x, 0, circleDiameter, circleDiameter);
+        x = x + circleDiameter;
+    }
+    
+    y = y + circleDiameter;
+    }
+}
     ellipse(0, height/2, circleDiameter, circleDiameter);
     ellipse(circleDiameter, height/2, circleDiameter, circleDiameter);
     ellipse(2*circleDiameter, height/2, circleDiameter, circleDiameter);
@@ -19,4 +31,3 @@ function draw() {
     ellipse(10*circleDiameter, height/2, circleDiameter, circleDiameter);
     ellipse(11*circleDiameter, height/2, circleDiameter, circleDiameter);
     ellipse(12*circleDiameter, height/2, circleDiameter, circleDiameter);
-}
