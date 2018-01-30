f3cfbefa65cea0211c21f6c9481b0a45f6b48794
Gnandeep Chintala <gnandeep.c25@gmail.com>
2018-01-30 22:05:50 +0000

    reversing position of circles

--- geometeric_pattern/main.js
+++ geometeric_pattern/main.js
@@ -10,8 +10,8 @@ function setup() {
 function draw() {
     var isShifted = false;
     
-    var y = 0;
-    while (y <= height) {
+    var y = height;
+    while (y >= 0) {
         
     var x;
     
@@ -26,7 +26,7 @@ function draw() {
         x = x + circleDiameter;
     }
     
-    y = y + circleRadius;
+    y = y - circleRadius;
     isShifted = !isShifted;
   }
 }
