a94d30e8355987ea92aaaa311abdb804b8882c50
Gnandeep Chintala <gnandeep.c25@gmail.com>
2018-01-30 22:34:18 +0000

    Added animation for geometeric pattern

--- geometeric_pattern/main.js
+++ geometeric_pattern/main.js
@@ -1,52 +1,51 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
-function setup() { 
-    createCanvas(480, 600);
-    circleDiameter = width/NUM_CIRCLES;
-    circleRadius = circleDiameter/2;
-}
-function draw() {
-  rVAL = 255;
+function setup() {
+  createCanvas(480, 600);
+
+  frameRate(5);
+
+  circleDiameter = width/NUM_CIRCLES;
+  circleRadius = circleDiameter/2;
+
+  rVal = 255;
   gVal = 0;
   bVal = 0;
-  
-    var isShifted = false;
-    
-    var y = height;
-    while (y >= 0) {
-        
+}
+
+function draw() {
+  var isShifted = false;
+
+  var y = height;
+  while (y >= 0) {
+
     var x;
-    
+
     if (isShifted) {
-        x = circleRadius;
+      x = circleRadius;
     } else {
-        x = 0;
+      x = 0;
     }
 
-    while(x <= width) {
-        fill(color(rVAL,gVal,bVal));
-        stroke(color(rVAL,gVal,bVal));
-        ellipse(x, y, circleDiameter, circleDiameter);
-        x = x + circleDiameter;
+    while (x <= width) {
+      stroke(color(rVal, gVal, bVal));
+      fill(color(rVal, gVal, bVal));
+      ellipse(x, y, circleDiameter, circleDiameter);
+      x = x + circleDiameter;
     }
-    
+
     y = y - circleRadius;
     isShifted = !isShifted;
-    
-    rVAL = rVAL - 2;
-    gVal = gVal + 7;
-    bVal = bVal + 3;
-   
+
+    rVal = (rVal + 254) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal + 3) % 256;
   }
-}
-function keyPressed() {
-  if (keyCode === 115 || keyCode === 83) {
-    saveCanvas('geometricPattern', 'png');
-  }
-  return false;
 }
